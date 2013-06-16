9a6d1f27a4f8ea5a37934285c5d85e1bddf4b746
Roger Camargo <huogerac@gmail.com>
2013-06-16 22:52:22 +1000

    TemplateView example

--- godjango/godjango/urls.py
+++ godjango/godjango/urls.py
@@ -20,3 +20,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += patterns('',
+    url(r'', include('todo.urls')),
+)
