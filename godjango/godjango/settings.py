560222b1dc522310dcc035a667a8f39f64b6d030
Roger Camargo <huogerac@gmail.com>
2013-06-17 21:03:44 +1000

    Customizing project admin with Grappelli

--- godjango/godjango/settings.py
+++ godjango/godjango/settings.py
@@ -77,8 +77,8 @@ STATICFILES_DIRS = (
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'django.contrib.staticfiles.finders.FileSystemFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
@@ -120,10 +120,11 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    #admin
+    'grappelli',    
+    'django.contrib.admin',
+
+    #site
     'contact',
     'todo',
 )
