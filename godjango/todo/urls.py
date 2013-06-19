e6e0ae7027fa48b5255a3b09fc412e3ccb05b71a
Roger Camargo <huogerac@gmail.com>
2013-06-19 19:36:28 +1000

    Using ListView

--- godjango/todo/urls.py
+++ godjango/todo/urls.py
@@ -1,12 +1,25 @@
 from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
+from django.views.generic import ListView
 
 from views import DisplayTaskView, DisplayTaskRedirectView
 
+from .models import Task
+
 urlpatterns = patterns('todo.views',
 
-    url(r'^(?P<task_id>\d+)-(?P<slug>[-\w]+)/$', DisplayTaskView.as_view()),
+    url(r'^(?P<task_id>\d+)-(?P<slug>[-\w]+)/$', DisplayTaskView.as_view(), name="task_page"),
     url(r'^(?P<task_id>\d+)/$', DisplayTaskRedirectView.as_view()),
-    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    
+    url(r'^tasklistview/$', ListView.as_view(
+                                model = Task,
+                                paginate_by = '5',
+                                queryset = Task.objects.all(),
+                                context_object_name = "tasks",
+                                template_name='task-listview.html')),
+    
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name="home_page"),
+    
+    
         
 )
