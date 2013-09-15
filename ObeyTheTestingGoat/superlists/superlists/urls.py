269f52e59d139138ad7dda26e7b2319621059684
Roger Camargo <huogerac@gmail.com>
2013-09-15 19:22:49 +1000

    First unit test and url mapping, dummy view

--- ObeyTheTestingGoat/superlists/superlists/urls.py
+++ ObeyTheTestingGoat/superlists/superlists/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'superlists.views.home', name='home'),
     # url(r'^superlists/', include('superlists.foo.urls')),
+	url(r'^$', 'lists.views.home_page', name='home'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
