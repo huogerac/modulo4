1a2f0f67131ce25be6a4e146e3c61ad51558a3b5
Roger Camargo <huogerac@gmail.com>
2013-09-23 19:44:32 +1000

    bugfix on functionaltests

--- ObeyTheTestingGoat/superlists/functional_tests/tests/test_list_item_validation.py
+++ ObeyTheTestingGoat/superlists/functional_tests/tests/test_list_item_validation.py
@@ -33,11 +33,12 @@ class ItemValidationTest(FunctionalTest):
 	def test_cannot_add_dulicate_items(self):
 		# Edith goes to the home page and starts a new list
 		self.browser.get(self.server_url)
-		self.browser.get_item_input_box().send_keys('Buy wellies\n')
+		self.get_item_input_box().send_keys('Buy wellies\n')
+		
 		self.check_for_row_in_list_table('1: Buy wellies')
 
 		# She accidentally tries to enter a duplicate item
-		self.browser.get_item_input_box().send_keys('Buy wellies\n')
+		self.get_item_input_box().send_keys('Buy wellies\n')
 
 		# She sees a helpful error message
 		self.check_for_row_in_list_table('1: Buy wellies')
